bdbe669dd19245d90cb48e2f1ba231c766518ad9
Dynameiz <hanz141204@gmail.com>
2024-07-25 14:30:56 +0700

    Change connection to railway

--- database/db.js
+++ database/db.js
@@ -2,10 +2,9 @@ var mysql2 = require('mysql2');
 
 var db = mysql2.createConnection({
     host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
 });
 
 db.connect();
